ece49663131abcf8db35303973a234513ce6ea3e
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2019-08-27 08:47:30 -0700

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is a test" << std::endl;
+	std::cout << "please input the first number you want to calculate" << std::endl;
+	int x;
+	int y;
+	int a;
+	int b;
+	int c;
+	int d;
+	std::cin >> x;
+	std::cout << "Please input the second number you want to calculate" << std::endl;
+	std::cin >> y;
+
+	std::cout << x << " + " << y << " = " << (double)x + (double)y << std::endl;
+	std::cout << x << " - " << y << " = " << (double)x - (double)y << std::endl;
+	std::cout << x << " * " << y << " = " << (double)x * (double)y << std::endl;
+	std::cout << x << " / " << y << " = " << (double)x / (double)y << std::endl;
+	return 0;
+}
