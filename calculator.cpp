97745d69fe4a82c676b91a3630113c7a485897a5
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2020-10-31 10:01:43 -0700

    made it actually act as a float
    special thanks to bench for helping me out lol

--- calculator.cpp
+++ calculator.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	std::cout << "This is a test" << std::endl;
 	std::cout << "please input the first number you want to calculate" << std::endl;
-	int x;
-	int y;
+	float x;
+	float y;
 	std::cin >> x;
 	std::cout << "Please input the second number you want to calculate" << std::endl;
 	std::cin >> y;
