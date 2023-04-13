d7fe2c949482c072ef0e3b85e4e72d8e54abb933
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2023-04-13 10:17:10 -0400

    Let you actually make a choice for math operation
    currently only basic arithmetic math is added but will add more advanced math later and possibly other features

--- calculator.cpp
+++ calculator.cpp
@@ -7,9 +7,10 @@ void function()
 	cout << "This is a test of my c++ coding ability" << endl;
 initialize:
 	double x; //using 64bit floating point numbers for variables
-	x = 0; // initialized as 0 to ensure its an integer
+	    x = 0; // initialized as 0 to ensure its an integer
 	double y;
-	y = 0;
+	    y = 0;
+	char math = 'h';
 for (;;) { //error handling for first number
 	cout << "please input the first number you want to calculate" << flush << endl; //flush is needed to verify that its cleared
 	cin >> x;
@@ -20,9 +21,25 @@ for (;;) { //error handling for first number
             continue;
         }
     else if (cin.good());
-        goto initialize0; //verified to be good and continues
+        break; //verified to be good and continues
 }
 initialize0:
+    cout << "what math operation would you like to use" << endl;
+    cin >> math;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        switch (math) {
+            case '+':
+                break;
+            case '-':
+                break;
+            case '*':
+                break;
+            case '/':
+                break;
+            default:
+                cout << "This operation is not recognized please use standard arithmetic math" << endl;
+                goto initialize;
+        }
 for (;;) { //error handling for second number
 	cout << "Please input the second number you want to calculate" << flush << endl;
 	cin >> y;
@@ -33,15 +50,38 @@ for (;;) { //error handling for second number
             continue;
         }
     else if (cin.good());
-        goto calculate; //continues to actual calculation
+        break; //continues to actual calculation
 }
 calculate: //function for calculation
+    switch (math) {
+        case '+':
+            goto addition;
+        case '-':
+            goto subtraction;
+        case '*':
+            goto multiplication;
+        case '/':
+            goto division;
+        default:
+            cout << "you shouldn't see this error, if you do you should make a detailed bug report, thanks!";
+            goto initialize;
+    }
+addition:
 	cout << x << " + " << y << " = " << (double)x + (double)y << endl;
+	cout << "" << endl;
+	    goto initialize;
+subtraction:
 	cout << x << " - " << y << " = " << (double)x - (double)y << endl;
+	cout << "" << endl;
+	    goto initialize;
+multiplication:
 	cout << x << " * " << y << " = " << (double)x * (double)y << endl;
+	cout << "" << endl;
+	    goto initialize;
+division:
 	cout << x << " / " << y << " = " << (double)x / (double)y << endl;
 	cout << "" << endl;
-	goto initialize; // Yes I'm using goto in a function to loop my code.
+	    goto initialize; // Yes I'm using goto in a function to loop my code.
 }
 
 int main()
