5b1698f96a189adc76d18b49e2444a44ff943ab6
TurboSonic1 <marcianocartagena@gmail.com>
2023-04-14 00:06:49 -0400

    Very work in progress, you can try it but it won't calculate ;(

--- calculator.cpp
+++ calculator.cpp
@@ -4,17 +4,9 @@
 #include <string>
 using namespace std;
 
-void function() 
-{
-	cout << "This is a test of my c++ coding ability" << endl;
-initialize:
-	double x; //using 64bit floating point numbers for variables
-	    x = 0; // initialized as 0 to ensure its an integer
-	double y;
-	    y = 0;
-	string longword;
+void startcalculation(double &x, double &z){
 for (;;) { //error handling for first number
-	cout << "please input the first number you want to calculate" << flush << endl; //flush is needed to verify that its cleared
+	cout << "Please enter the number(s) you want to have calculated" << flush << endl;
 	cin >> x;
 	if (cin.fail()) { //if it fails
             cerr << "Sorry, I cannot read that. Please try again." << endl;
@@ -25,8 +17,50 @@ for (;;) { //error handling for first number
     else if (cin.good());
         break; //verified to be good and continues
 }
-operation:
-    cout << "what math operation would you like to use" << endl;
+}
+
+
+void calculation(char &math, double x, double y, double z) {
+switch (math) { //Actual Calculations
+    case '+':
+        cout << x << " + " << y << " = " << (double)x + (double)y << endl;
+	    cout << "" << endl;
+        z = (double)x + (double)y;
+	        break;
+    case '-':
+        cout << x << " - " << y << " = " << (double)x - (double)y << endl;
+	    cout << "" << endl;
+        z = x - y;
+	        break;
+    case '*':
+        cout << x << " * " << y << " = " << (double)x * (double)y << endl;
+	    cout << "" << endl;
+        z = x * y;
+	        break;
+    case '/':
+        cout << x << " / " << y << " = " << (double)x / (double)y << endl;
+	    cout << "" << endl;
+        z = x / y;
+	        break;
+	case 's':
+	    cout << x << "√(" << y << ")" << " = " << (double)x * sqrt(y);
+	    cout << "" << endl;
+        z = x * sqrt(y);
+	        break;
+    case '^':
+        cout << x << "^" << y << "=" << pow(x, y);
+        cout << "" << endl;
+        z = pow(x,y);
+            break;
+    default:
+        cout << "you shouldn't see this error, if you do you should make a detailed bug report, thanks!";
+            break;
+    }
+}
+
+
+void operation(string &longword, double x, double y, double z){
+cout << "what math operation would you like to use" << endl;
     cin >> longword;
     char math = longword[0]; //only takes first letter of what the user types to prevents errors
         switch (math) {
@@ -44,10 +78,10 @@ operation:
                 break;
             default:
                 cout << "This operation is not recognized please use standard arithmetic math or squareroots/exponents" << endl;
-                goto operation;
+                operation;
         }
 for (;;) { //error handling for second number
-	cout << "Please input the second number you want to calculate" << flush << endl;
+	cout << "Please enter the number(s) you want to have calculated" << flush << endl; //flush is needed to verify that its cleared
 	cin >> y;
 	if (cin.fail()) { //if it fails
             cerr << "Sorry, I cannot read that. Please try again." << endl;
@@ -56,43 +90,36 @@ for (;;) { //error handling for second number
             continue;
         }
     else if (cin.good());
-        break; //continues to actual calculation
+        calculation(math, x, y, z);; //verified to be good and continues
 }
-//Actual Calculations
-switch (math) {
-    case '+':
-        cout << x << " + " << y << " = " << (double)x + (double)y << endl;
-	    cout << "" << endl;
-	        break;
-    case '-':
-        cout << x << " - " << y << " = " << (double)x - (double)y << endl;
-	    cout << "" << endl;
-	        break;
-    case '*':
-        cout << x << " * " << y << " = " << (double)x * (double)y << endl;
-	    cout << "" << endl;
-	        break;
-    case '/':
-        cout << x << " / " << y << " = " << (double)x / (double)y << endl;
-	    cout << "" << endl;
-	        break;
-	case 's':
-	    cout << x << "√(" << y << ")" << " = " << (double)x * sqrt(y);
-	    cout << "" << endl;
-	        break;
-    case '^':
-        cout << x << "^" << y << "=" << pow(x, y);
-        cout << "" << endl;
-            break;
-    default:
-        cout << "you shouldn't see this error, if you do you should make a detailed bug report, thanks!";
-            break;
-    }
-goto initialize;
+}
+
+
+void clearvalues(string longword,double x,double y,double z)
+{
+    x = 0;
+    y = 0;
+    z = 0;
+    startcalculation(x, z);
+    operation(longword, x, y, z);
 }
 
 int main()
 {
-    function ();
+cout << "This is a test of my c++ coding ability" << endl;
+bool loop = 1;
+if (loop>0)
+{
+	double x; //using 64bit floating point numbers for variables
+	    x = 0; // initialized as 0 to ensure its an integer
+	double y;
+	    y = 0;
+    double z; //currently not in use, for future update
+        z = 0;
+	string longword;
+    startcalculation(x, z);
+    operation(longword, x, y, z);
+    clearvalues(longword, x, y, z);
     return 0;
+}
 }
